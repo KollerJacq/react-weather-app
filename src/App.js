e177a1f4c649e11fde5786ee7d9ff7c6f44e7b14
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-12-28 13:51:36 +0100

    Added the GitHub repo and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a href="https://github.com/KollerJacq" target="_blank">
+          Jacqueline Koller
+        </a>{" "}
+        an is open-sourced on{" "}
+        <a
+          href="https://github.com/KollerJacq/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 export default App;
