f5e71362a187f5bdd8b7689ce0baf6b09291d688
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-12-28 14:04:21 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by
-        <a
-          href="https://github.com/KollerJacq"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Jacqueline Koller
-        </a>{" "}
-        an is open-sourced on{" "}
-        <a
-          href="https://github.com/KollerJacq/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/KollerJacq"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jacqueline Koller
+          </a>{" "}
+          an is open-sourced on{" "}
+          <a
+            href="https://github.com/KollerJacq/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
