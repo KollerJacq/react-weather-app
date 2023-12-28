3dfde8663f1c24a11b4511128bfe6cc1a18103b0
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-12-28 13:54:01 +0100

    Improved the links

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by
-        <a href="https://github.com/KollerJacq" target="_blank">
+        <a
+          href="https://github.com/KollerJacq"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Jacqueline Koller
         </a>{" "}
         an is open-sourced on{" "}
         <a
           href="https://github.com/KollerJacq/react-weather-app"
           target="_blank"
+          rel="noopener noreferrer"
         >
           GitHub
         </a>
